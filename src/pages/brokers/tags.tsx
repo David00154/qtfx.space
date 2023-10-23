fdc98c6d6105975c8bfdc92ac9b5223c06a46f39
david00154 <davidbriggs00154@gmail.com>
2023-10-23 11:49:07 +0000

    initial commit

---
+++ src/pages/brokers/tags.tsx
@@ -0,0 +1,52 @@
+import jpg from "../../assets/images/1.jpg";
+export const SeoMeta = () => (
+  <>
+    <meta name="author" content="Jeffrey Chadwick, JeffreyChadwick" />
+    <meta name="keywords" content="Jeffrey Chadwick, JeffreyChadwick" />
+    <meta
+      name="google-site-verification"
+      content="1HYM4qZvOFOBB5gaRNWWRDuX39v-4eoBn2MQCMqBGgk"
+    />
+    <meta
+      name="description"
+      content="Jeffrey Chadwick is a CERTIFIED FINANCIAL PLANNER™ professional. He has also earned his Chartered Financial Consultant Designation®. In addition to these designations, Jeffrey Chadwick has his Series 7, Series 63, and Series 9 licenses. He is also licensed in the State of Alabama to offer life insurance and annuity products. Jonathan specializes in Estate, Retirement, and Investment Planning.."
+    />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
+    <link rel="stylesheet" type="text/css" href="assets/dist/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/dist/responsive.css" />
+    <link rel="canonical" href="index.html" />
+    <link rel="shortcut icon" href={jpg} />
+    <link rel="apple-touch-icon-precomposed" href={jpg} />
+    {/* <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    /> */}
+  </>
+);
+
+export const SeoEnd = () => (
+  <>
+    <script
+      data-cfasync="false"
+      src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
+    ></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.easing.js"></script>
+    <script src="assets/js/countto.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/swiper.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/jquery-validate.js"></script>
+    <script src="assets/js/smooth-scroll.min.js"></script>
+    <script src="assets/js/animated-headline.js"></script>
+    <script src="assets/js/plugin.js"></script>
+    <script src="assets/js/shortcodes.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/slider-title.min.js"></script>
+    <script src="index.js"></script>
+  </>
+);
